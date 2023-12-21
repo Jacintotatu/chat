594dcdb0c6b003e18eeececf5c343820725fd702
Jacintotatu <jacintoconesa@hotmail.com>
2023-12-21 10:28:51 +0100

    Create proyecto_peluqueria.py

---
+++ proyecto_peluqueria.py
@@ -0,0 +1,5 @@
+#import xlsxwriter
+
+#precios = {"Depilación": 21, "Manicura_1": 20, "Manicura_2": 8, "Facial": 30, 
+           #"Corporal_1": 25, "Corporal_2": 15, "Pedicura": 18}
+print("precios")
